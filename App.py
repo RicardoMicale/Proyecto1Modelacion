34b3fb84d5d280deeb317414986e9368821e790c
Ricardo Micale <ricardomicale@gmail.com>
2023-06-13 20:31:40 -0400

    clase grafo

--- App.py
+++ App.py
@@ -8,10 +8,10 @@ from Graph import Graph
 # LOADING DATA
 def loadAirports():
   '''
-  Loads the data from the json in the data folder
-  Loops through it and creates airport objects
-  Makes the code as the key for the object in a dictionary
-  Returns the airport dictionary
+  Loads the data from the json in the data folder,
+  loops through it and creates airport objects,
+  makes the code as the key for the object in a dictionary
+  returns the airport dictionary
   '''
   # AIRPORTS
   airportsData = open('./data/airportData.json')
@@ -28,10 +28,10 @@ def loadAirports():
 
 def loadNodes(airports):
   '''
-  Loads the data from the json in the data folder
-  Loops through it and creates node objects
-  Appends the node objects into an array
-  Returns the node array
+  Loads the data from the json in the data folder,
+  loops through it and creates node objects,
+  appends the node objects into an array
+  and returns the node array
   '''
   # NODES
   travelData = open('./data/travelData.json')
@@ -47,11 +47,25 @@ def loadNodes(airports):
   travelData.close()
   return _travel
 
+def makeGraph(nodes, graph, airports):
+  '''
+  Takes in a graph object and a list of nodes,
+  loops through the nodes
+  and creates edges on the graph instance
+  '''
+  for node in nodes:
+    graph.addEdge(node.start, node.end, node.cost, airports)
+
 def runApp():
   '''
   Runs the app
   '''
   airports = loadAirports()
   travels = loadNodes(airports)
-
+  # Creating graph object
+  graph = Graph()
+  makeGraph(travels, graph, airports)
+  graph.printData()
   #for i in travels: i.printData()
+
+runApp()
--- Graph.py
+++ Graph.py
@@ -1,3 +1,17 @@
 class Graph:
-  def __init__(self, title):
-    self.title = title
+  def __init__(self):
+    self.graph = dict()
+
+  def addEdge(self, start, end, cost, airports):
+    # If the node is not in the graph
+    # it creates a sublist under the key
+    if start not in self.graph: self.graph[start] = []
+    if end not in self.graph: self.graph[end] = []
+
+    # appends a tuple with the destination, cost and requirement for a visa
+    self.graph[start].append((end, float(cost), airports[end].visaRequired))
+    self.graph[end].append((start, float(cost), airports[start].visaRequired))
+    return self.graph
+
+  def printData(self):
+    for key in self.graph: print(f'{key} --> {self.graph[key]}')
